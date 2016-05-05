c92e14907c4321cf87ca632199171e218dbb8468
willwtang <willwyatttang@gmail.com>
2016-05-04 23:00:16 -0700

    Finished bare minimum requirements

--- test/spec/PlayerViewSpec.js
+++ test/spec/PlayerViewSpec.js
@@ -31,7 +31,7 @@ describe('PlayerView', function() {
     expect(appView.playerView.model).to.equal(library.at(0));
   });
 
-  xit('dequeues a song when finished playing & plays the next song', function() {
+  it('dequeues a song when finished playing & plays the next song', function() {
     var firstSong = library.at(0);
     var secondSong = library.at(1);
     var thirdSong = library.at(2);
@@ -43,10 +43,11 @@ describe('PlayerView', function() {
     // play the first song
     songQueue.playFirst();
     expect(appView.playerView.model).to.equal(firstSong);
+
     // Simulate the end of the first song
     $(appView.playerView.el).trigger('ended');
     expect(appView.playerView.model).to.equal(secondSong);
-    // Simulate the end of the second song
+    // // Simulate the end of the second song
     $(appView.playerView.el).trigger('ended');
     expect(appView.playerView.model).to.equal(thirdSong);
   });
