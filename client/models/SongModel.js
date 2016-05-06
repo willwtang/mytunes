e74925ad103daaf316de3569d8380020c1603ce5
willwtang <willwyatttang@gmail.com>
2016-05-05 17:24:52 -0700

    Finished parse

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,9 +1,13 @@
 // SongModel.js - Defines a backbone model class for songs.
 var SongModel = Backbone.Model.extend({
+  defaults: {
+    playCount: 0
+  },
 
   play: function() {
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
+    this.set('playCount', this.get('playCount') + 1);
   },
 
   enqueue: function() {
--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -3,12 +3,13 @@ var LibraryEntryView = Backbone.View.extend({
 
   tagName: 'tr',
 
-  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+  template: _.template('<td>(<%= playCount %>)(<%= artist %>)</td><td><%= title %></td>'),
 
   events: {
     'click': function() {
       this.model.play();
       this.model.enqueue();
+      this.render();
     }
   },
 
